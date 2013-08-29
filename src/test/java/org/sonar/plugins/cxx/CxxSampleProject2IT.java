2ea331a954a092d86237de50ec45183464c7a31e
Norbert Kosmal <norbert.kosmal@gmail.com>
2013-08-29 20:00:48 +0200

    Test for ignoring coverage

---
+++ src/test/java/org/sonar/plugins/cxx/CxxSampleProject2IT.java
@@ -0,0 +1,90 @@
+/*
+ * Sonar Cxx Plugin, open source software quality management tool.
+ * Copyright (C) 2010 - 2011, Neticoa SAS France - Tous droits reserves.
+ * Author(s) : Franck Bonin, Neticoa SAS France.
+ *
+ * Sonar Cxx Plugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Sonar Cxx Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Sonar Cxx Plugin; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.cxx;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.sonar.wsclient.Sonar;
+import org.sonar.wsclient.services.Measure;
+import org.sonar.wsclient.services.Resource;
+import org.sonar.wsclient.services.ResourceQuery;
+
+//@Ignore
+public class CxxSampleProject2IT {
+
+  private static Sonar sonar;
+  private static final String PROJECT_SAMPLE = "CxxPlugin:Sample3";
+
+  @BeforeClass
+  public static void buildServer() {
+    sonar = Sonar.create("http://localhost:9000");
+  }
+
+  @Test
+  public void coverageMetrics() {
+    String[] metricNames =
+      {
+       "coverage", "line_coverage", "branch_coverage"
+      };
+
+    for(int i = 0; i < metricNames.length; ++i){
+      assertNull(getProjectMeasure(metricNames[i]));
+    }
+
+  }
+  
+  @Test
+  public void projectsMetrics() {
+    String[] metricNames =
+      {"ncloc", "lines",
+       "files", "directories", "functions",
+       "comment_lines_density", "comment_lines", "comment_blank_lines", "commented_out_code_lines",
+       "duplicated_lines_density", "duplicated_lines", "duplicated_blocks", "duplicated_files",
+       "complexity", "function_complexity",
+     //  "coverage", "line_coverage", "branch_coverage",
+       "test_success_density", "test_failures", "test_errors", "tests"
+      };
+
+    double[] values = new double[metricNames.length];
+    for(int i = 0; i < metricNames.length; ++i){
+      values[i] = getProjectMeasure(metricNames[i]).getValue();
+    }
+
+    double[] expectedValues = {59.0, 121.0,
+                               4.0, 3.0, 5.0,
+                               28.9, 24.0, 10.0, 0.0,
+                               67.8, 82.0, 2.0, 2.0,
+                               7.0, 1.4,
+       //                        0.0, 0.0, 0.0,
+                               60.0, 2.0, 0.0, 5.0};
+
+    assertThat(values, is(expectedValues));
+  }
+
+
+  private Measure getProjectMeasure(String metricKey) {
+    Resource resource = sonar.find(ResourceQuery.createForMetrics(PROJECT_SAMPLE, metricKey));
+    return resource!=null ? resource.getMeasure(metricKey) : null;
+  }
+}
